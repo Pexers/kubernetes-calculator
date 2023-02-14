7ec6e8a4248e31c678951f9a2b6dd232b6f0f8c9
Pexers <pedro-rodri@outlook.com>
2023-02-14 17:34:25 +0000

    Add 'addition' service and link it to 'calculator' service using Docker-Compose file

--- services/calculator/src/api/routes/calculator.js
+++ services/calculator/src/api/routes/calculator.js
@@ -5,6 +5,98 @@ const { apiCall } = require("../../common.js");
 
 const router = express.Router();
 
+const expressionMap = {
+  addition: {
+    isValid: isValidBinaryExpression,
+    solve: async (expression, options) => {
+      return solveBinaryExpression("addition", expression, options);
+    }
+  },
+  division: {
+    isValid: isValidBinaryExpression,
+    solve: async (expression, options) => {
+      return solveBinaryExpression("division", expression, options);
+    }
+  },
+  multiplication: {
+    isValid: isValidBinaryExpression,
+    solve: async (expression, options) => {
+      return solveBinaryExpression("multiplication", expression, options);
+    }
+  },
+  number: {
+    isValid: isValidUnaryExpression,
+    solve: async (expression, options) => {
+      return solveExpression(expression.value, options);
+    }
+  },
+  subtraction: {
+    isValid: isValidBinaryExpression,
+    solve: async (expression, options) => {
+      return solveBinaryExpression("subtraction", expression, options);
+    }
+  }
+};
+
+const expressionTypes = Object.keys(expressionMap);
+
+function isNumber(expression) {
+  return typeof expression === "number";
+}
+
+function isValidExpressionType(type) {
+  return !!(typeof type === "string" && expressionTypes.find(e => e === type));
+}
+
+function isValidUnaryExpression(expression) {
+  return isValidExpression(expression.value);
+}
+
+function isValidBinaryExpression(expression) {
+  return (
+    isValidExpression(expression.left) && isValidExpression(expression.right)
+  );
+}
+
+function isValidExpression(expression) {
+  if (isNumber(expression)) {
+    return true;
+  }
+
+  const type = expression && expression.type;
+
+  if (!isValidExpressionType(type)) {
+    return false;
+  }
+
+  return expressionMap[type].isValid(expression);
+}
+
+async function solveBinaryExpression(serviceName, expression, options) {
+  const left = await solveExpression(expression.left, options);
+  const right = await solveExpression(expression.right, options);
+
+  const response = await apiCall(
+    serviceName,
+    {
+      type: serviceName,
+      left,
+      right
+    },
+    options
+  );
+
+  return response.value;
+}
+
+async function solveExpression(expression, options) {
+  if (isNumber(expression)) {
+    return expression;
+  }
+
+  return expressionMap[expression.type].solve(expression, options);
+}
+
 router.post(
   "/",
   asyncWrap(async (request, response) => {
